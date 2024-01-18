790dd12de5ae254d2e97e0beaf0f86c72df85e82
finnzao <lipefelipe403@hotmail.com>
2024-01-17 21:34:33 -0300

    feat : add enviroment config production

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,7 +1,11 @@
 import { knex } from 'knex';
-
+import 'dotenv/config'
+import pg from 'pg'
 import { development, production, test } from './Environments';
 
+if (process.env.NODE_ENV === 'production') {
+  pg.types.setTypeParser(28, 'text', parseInt)
+}
 
 const getEnvironment = () => {
   switch (process.env.NODE_ENV) {

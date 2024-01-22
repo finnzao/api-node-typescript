b7f71dd06c050938d76859091c2f83bef15504a1
finnzao <lipefelipe403@hotmail.com>
2024-01-21 21:38:10 -0300

    Cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { router } from './routes';
-
+import cors from 'cors'
 const server = express()
 
 
+server.use(cors({
+    origin: process.env.ENABLED_CORS?.split(';') || []
+}))
 server.use(express.json())
 
 server.use(router)
